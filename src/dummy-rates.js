1fb5714a38a401de55422939d74ce54249b0c690
Samuel Goldfarb <shgoldfarb@uc.cl>
2018-10-28 18:55:11 -0300

    comly with aribnb 2

--- src/dummy-rates.js
+++ src/dummy-rates.js
@@ -1,9 +1,9 @@
 const rates = {
-    usd: 1,
-    clp: 687.6,
-    ars: 36.77,
-    eur: 0.88,
-    jpy: 11.9
-}
+  usd: 1,
+  clp: 687.6,
+  ars: 36.77,
+  eur: 0.88,
+  jpy: 11.9,
+};
 
 export default rates;
