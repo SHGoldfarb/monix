61d3689271a4765e2f37841de55885a384768011
Samuel Goldfarb <shgoldfarb@uc.cl>
2018-10-28 14:40:52 -0300

    add eslint

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import View from './View'
-import rates from './dummy-rates'
+import View from './View';
+import rates from './dummy-rates';
 import { setRate1, setRate2 } from './actions.js';
 import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
-    const rate_names = Object.keys(rates);
+    const rateNames = Object.keys(rates);
     const rate1 = {
-      name: rate_names[0],
-      rate: rates[rate_names[0]]
+      name: rateNames[0],
+      rate: rates[rateNames[0]],
     };
     const rate2 = {
-      name: rate_names[1],
-      rate: rates[rate_names[1]]
-    };
-    this.state = {
-      rate1,
-      rate2
+      name: rateNames[1],
+      rate: rates[rateNames[1]],
     };
 
     props.onRatesChange(rate1, rate2);
@@ -34,9 +30,9 @@ class App extends Component {
 
 const mapDispatchtoProps = dispatch => ({
   onRatesChange: (rate1, rate2) => {
-    dispatch(setRate1(rate1))
-    dispatch(setRate2(rate2))
-  }
+    dispatch(setRate1(rate1));
+    dispatch(setRate2(rate2));
+  },
 });
 
 export default connect(() => { }, mapDispatchtoProps)(App);
